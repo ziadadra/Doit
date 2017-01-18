f904c6e3975bbbb2a450d32901287b55aff4aae7
ziad adra <ziadadra@gmail.com>
2017-01-18 21:15:22 +0200

    put the text label need the important filed

--- Doit/ViewController.swift
+++ Doit/ViewController.swift
@@ -8,18 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITableViewDelegate,
+    UITableViewDataSource
+{
+    @IBOutlet weak var tableView: UITableView!
+    var tasks : [Task] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tasks = makeTasks()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+        
     }
-
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let task = tasks[indexPath.row]
+        if task.important {
+            cell.textLabel?.text = "❗️\(task.name)"
+        }else {
+            cell.textLabel?.text = task.name
+        }
+        
+        
+        
+        return cell
+    }
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Walk the dog"
+        task1.important = false
+        
+        let task2 = Task()
+        task2.name = "Buy Cheese"
+        task2.important = true
+        
+        let task3 = Task()
+        task3.name = "Mow the lawn"
+        task3.important = false
+        return [task1,task2,task3]
+        
+        
+        
+        
+    }
+    @IBAction func plusTapped(_ sender: Any) {
+        performSegue(withIdentifier: "addSegue", sender: nil)
+    }
 }
 
